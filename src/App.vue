7ed1aadd45ca19b093bfc6001b6073e521396427
haoxf <hxf098@126.com>
2022-11-25 18:16:08 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>
+    kaiyuan10nian_Client
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+</style>
